10500c3251ee40d2188c4e188af11965ed1bd532
bs4p <bambang.s4p@gmail.com>
2023-08-24 22:48:05 +0700

    add render title

--- server/routes/chatRouter.js
+++ server/routes/chatRouter.js
@@ -3,7 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('chat');
+  res.render('chat', {
+    title: 'Chat',
+  });
 });
 
 export default router;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,7 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('user');
+  res.render('user', {
+    title: 'User',
+  });
 });
 
 export default router;
