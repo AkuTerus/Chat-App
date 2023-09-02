adb4da3d9736e34b5d7410561c778aedbd9e3a7a
bs4p <bambang.s4p@gmail.com>
2023-09-02 23:39:19 +0700

    feat: add chat rooms views functionality

--- server/models/chatModel.js
+++ server/models/chatModel.js
@@ -3,22 +3,23 @@ import db from '../config/database.js';
 
 const table = 'rooms';
 
-async function getUserIdByToken(token) {
-  const query = `SELECT u.id
+export async function getUserByToken(token) {
+  const query = `SELECT u.id, u.name, u.avatar
     FROM users u
     INNER JOIN login_details ld ON ld.email=u.email
-    WHERE token=?
+    WHERE token = ?
   `;
-  const [[userid]] = await db.execute(query, [token]);
-  return userid ? true : false;
+  const [[user]] = await db.execute(query, [token]);
+  return user;
 }
 
-async function isExistPartner(id) {
+async function getPartnerById(id) {
   const query = `SELECT * FROM users WHERE id = ?`;
-  const [[userid]] = await db.execute(query, [id]);
-  return userid ? true : false;
+  const [[partner]] = await db.execute(query, [id]);
+  return partner;
 }
 
+// export
 export async function createRoom(token, partnerid) {
   if (!token || !partnerid) {
     return {
@@ -27,16 +28,16 @@ export async function createRoom(token, partnerid) {
     };
   }
 
-  const userid = await getUserIdByToken(token);
-  if (!userid) {
+  const user = await getUserByToken(token);
+  if (!user) {
     return {
       code: 403,
       message: 'Unauthorized user to perform action',
     };
   }
-  console.log(`userid: ${userid}`);
+  console.log(`user.id: ${user.id}`);
 
-  const partner = await isExistPartner(partnerid);
+  const partner = await getPartnerById(partnerid);
   if (!partner) {
     return {
       code: 403,
@@ -53,7 +54,7 @@ export async function createRoom(token, partnerid) {
   console.log(`roomid: ${roomid}`);
 
   const queryParticipant = `INSERT INTO participants (room_id,user_id) VALUES (?,?)`;
-  const insertUser1 = await db.execute(queryParticipant, [roomid, userid]);
+  const insertUser1 = await db.execute(queryParticipant, [roomid, user.id]);
   const insertUser2 = await db.execute(queryParticipant, [roomid, partnerid]);
 
   return {
@@ -61,4 +62,38 @@ export async function createRoom(token, partnerid) {
     message: 'Room created succesfully',
     roomName: roomName,
   };
+}
+
+export async function getRoomByName(name) {
+  const query = `SELECT * FROM rooms WHERE name = ?`;
+  const [[room]] = await db.execute(query, [name]);
+  return room;
+}
+
+// export
+export async function getPartnerByRoomId(roomid, userid) {
+  const query = `
+    SELECT u.id, u.name, u.avatar
+    FROM users u
+    INNER JOIN participants p ON p.user_id = u.id
+    INNER JOIN rooms r ON r.id = p.room_id
+    WHERE r.id = ? AND u.id != ?
+  `;
+
+  const [[partner]] = await db.execute(query, [roomid, userid]);
+  return partner;
+}
+
+export async function getMessagesByRoomId(roomid) {
+  const query = `
+    SELECT 
+      m.id, m.sender_id, m.message, m.sent_at
+    FROM messages m
+    INNER JOIN rooms r ON r.id=m.room_id
+    WHERE r.id = ?
+    ORDER BY m.id
+  `;
+
+  const [messages] = await db.execute(query, [roomid]);
+  return messages;
 }
--- server/routes/chatRouter.js
+++ server/routes/chatRouter.js
@@ -1,11 +1,29 @@
 import express from 'express';
-import { createRoom } from '../models/chatModel.js';
+import {
+  createRoom,
+  getMessagesByRoomId,
+  getPartnerByRoomId,
+  getRoomByName,
+  getUserByToken,
+} from '../models/chatModel.js';
 
 const router = express.Router();
 
-router.get('/:roomName', (req, res) => {
+router.get('/:roomName', async (req, res) => {
+  const room = await getRoomByName(req.params.roomName);
+  if (!room) {
+    res.sendStatus(403);
+  }
+
+  const user = await getUserByToken(req.session.token);
+  const partner = await getPartnerByRoomId(room.id, user.id);
+  const messages = await getMessagesByRoomId(room.id);
+  console.log(messages);
   res.render('chat', {
     title: 'Chat',
+    user: user,
+    partner: partner,
+    messages: messages,
   });
 });
 
