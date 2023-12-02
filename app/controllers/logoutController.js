cef46fa8963361d76be2175db2c7f3c61b5e3bd4
bs4p <bambang.s4p@gmail.com>
2023-12-02 22:37:15 +0700

    remove optional error callback on session destroy

--- app/controllers/logoutController.js
+++ app/controllers/logoutController.js
@@ -2,7 +2,7 @@ export default {
   // GET /logout
   index: async (req, res) => {
     req.session.login = undefined;
-    req.session.destroy((err) => console.error(err));
+    req.session.destroy();
     res.redirect('/login');
   },
 };
