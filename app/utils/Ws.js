a0805fae3b97b781d6aee1662d4a877b070810a1
bs4p <bambang.s4p@gmail.com>
2023-12-09 22:49:38 +0700

    fix user login not auto update their own chats online status

--- app/utils/Ws.js
+++ app/utils/Ws.js
@@ -2,68 +2,65 @@ class Ws {
   users = [];
 
   connect(socket) {
-    socket.on('disconnect', () => this.def__OnDisconnect(socket));
-    socket.on('__:init', (data) => this.def__OnInit(socket, data));
-    socket.on('__:user_init_rooms', (data) => this.def__OnUserInitRooms(socket, data));
+    socket.on('disconnect', () => {
+      console.log('❌❌❌ on disconnect');
+      console.log(`| socket.userId = ${socket.userId}`);
+      console.log(`| before remove (this.users) :`);
+      console.log(this.users);
 
-    socket.on('chat:init', (data) => this.chatOnInit(socket, data));
-    socket.on('chat:send_message', (data) => this.chatOnSendMessage(socket, data));
-  }
+      const userData = this.users.find((u) => u.userId == socket.userId && u.socketId == socket.id); // save user data for emitting
+      if (!userData) return;
 
-  def__OnInit(socket, data) {
-    console.log('✅✅✅__:init');
-    socket.userId = data.userId;
-    console.log(`| socket.userId = ${socket.userId}`);
+      console.log('userData');
+      console.log(userData);
 
-    // let userData = this.users.find((u) => u.userId == data.userId);
-    // if (!userData) {
-    const userData = {
-      socketId: socket.id,
-      userId: data.userId,
-      userName: data.userName,
-    };
-    this.users.push(userData);
-    // }
+      this.users = this.users.filter((u) => u.socketId != userData.socketId); // remove socket from users
 
-    socket.broadcast.emit('user:online', userData);
-    console.log('| this.users');
-    console.log(this.users);
-  }
+      console.log(`| after remove (this.users) :`);
+      console.log(this.users);
 
-  def__OnDisconnect(socket) {
-    console.log('❌❌❌ on disconnect');
-    console.log(`| socket.userId = ${socket.userId}`);
-    console.log(`| before remove (this.users) :`);
-    console.log(this.users);
+      socket.broadcast.emit('user:offline', userData);
+    });
 
-    const userData = this.users.find((u) => u.userId == socket.userId && u.socketId == socket.id); // save user data for emitting
-    if (!userData) return;
+    socket.on('__:init', (data) => {
+      socket.emit('emit1', 'test');
 
-    console.log('userData');
-    console.log(userData);
+      console.log('✅✅✅__:init');
+      socket.userId = data.userId;
+      console.log(`| socket.userId = ${socket.userId}`);
 
-    this.users = this.users.filter((u) => u.socketId != userData.socketId); // remove socket from users
+      // let userData = this.users.find((u) => u.userId == data.userId);
+      // if (!userData) {
+      const userData = {
+        socketId: socket.id,
+        userId: data.userId,
+        userName: data.userName,
+      };
+      this.users.push(userData);
+      // }
 
-    console.log(`| after remove (this.users) :`);
-    console.log(this.users);
+      const rooms = data.rooms.map((room) => room.roomname);
+      socket.join(rooms);
 
-    socket.broadcast.emit('user:offline', userData);
-  }
+      const roomuserids = data.rooms.map((room) => room.roomuserid);
+      const roomuseridonlines = this.users.filter((u) => roomuserids.includes(u.userId)).map((u) => u.userId);
+      socket.emit('user:get_user_online', roomuseridonlines);
 
-  def__OnUserInitRooms(socket, data) {
-    // console.log('__:user_init_rooms');
-    socket.join(data.socketRooms);
-  }
+      socket.broadcast.emit('user:online', userData);
+      console.log('| this.users');
+      console.log(this.users);
+    });
 
-  chatOnInit(socket, data) {
-    socket.join(data.socketRoom);
-  }
+    socket.on('chat:init', (data) => {
+      socket.join(data.socketRoom);
+    });
 
-  chatOnSendMessage(socket, data) {
-    const partner = this.users.find((u) => u.userId == data.partnerid);
-    console.log(data);
-    console.log(partner);
-    socket.to(data.socketRoom).emit('chat:new_message', data);
+    socket.on('chat:send_message', (data) => {
+      const partner = this.users.find((u) => u.userId == data.partnerid);
+      console.log(data);
+      console.log(partner);
+      socket.to(data.socketRoom).emit('chat:new_message', data);
+    });
   }
 }
 
